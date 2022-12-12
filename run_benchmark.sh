dfb4ad9176de1ec99c99cae842f84e4545601565
Jeroen van Dijk <jeroen11dijk@gmail.com>
2022-12-12 15:00:40 +0100

    remove main

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 repo=/data/BCP-paper
-#PYTHONPATH=$repo:$PYTHONPATH python3.9 $repo/python/benchmarks/comparison_25percent_1teams.py
-#PYTHONPATH=$repo:$PYTHONPATH python3.9 $repo/python/benchmarks/comparison_25percent_3teams.py
-#PYTHONPATH=$repo:$PYTHONPATH python3.9 $repo/python/benchmarks/comparison_25percent_6teams.py
-#PYTHONPATH=$repo:$PYTHONPATH python3.9 $repo/python/benchmarks/comparison_25percent_12teams.py
-#PYTHONPATH=$repo:$PYTHONPATH python3.9 $repo/python/benchmarks/comparison_warehouse.py
-PYTHONPATH=$repo:$PYTHONPATH python3.9 $repo/python/benchmarks/main.py
+PYTHONPATH=$repo:$PYTHONPATH python3.9 $repo/python/benchmarks/comparison_25percent_1teams.py
+PYTHONPATH=$repo:$PYTHONPATH python3.9 $repo/python/benchmarks/comparison_25percent_3teams.py
+PYTHONPATH=$repo:$PYTHONPATH python3.9 $repo/python/benchmarks/comparison_25percent_6teams.py
+PYTHONPATH=$repo:$PYTHONPATH python3.9 $repo/python/benchmarks/comparison_25percent_12teams.py
+PYTHONPATH=$repo:$PYTHONPATH python3.9 $repo/python/benchmarks/comparison_warehouse.py
