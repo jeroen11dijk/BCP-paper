7f48167463f4065480b1899884d7e7e3e911f4bb
Jeroen van Dijk <jeroen11dijk@gmail.com>
2022-12-12 14:42:42 +0100

    remove stuff

--- python/benchmarks/comparison/src/astar/astar_agent.py
+++
@@ -1,9 +0,0 @@
-from python.benchmarks.comparison.src.data.agent import Agent
-from python.benchmarks.comparison.src.data.vertex import Vertex
-
-
-class AstarAgent(Agent):
-    def __init__(self, id: str, start: Vertex, goal: Vertex) -> None:
-        super().__init__(id)
-        self.start = start
-        self.goal = goal
--- python/benchmarks/comparison/src/cbm/cbm_ctnode.py
+++
@@ -1,6 +0,0 @@
-from python.benchmarks.comparison.src.ctnode import CTNode
-
-
-class CBMMakespanCTNode(CTNode):
-    def update_costs(self) -> None:
-        self.cost = max(self.cost, self.full_solution.get_cost())
